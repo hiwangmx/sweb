0d4bca3b4c36881393bf96548109dfc43bc597c1
hiwangmx <hiwangmx@gmail.com>
2015-01-17 14:47:11 +0800

    action init

---
+++ src/main/java/com/star/sweb/action/BaseAction.java
@@ -0,0 +1,9 @@
+package com.star.sweb.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BaseAction extends ActionSupport{
+
+	private static final long serialVersionUID = 1L;
+
+}
